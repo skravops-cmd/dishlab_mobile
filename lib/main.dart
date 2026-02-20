062af0120c5cf5d1abd8cd327e0c6057456a9dc4
skravco-cmd <skravco92@hotmail.com>
2026-02-20 23:35:58 +0100

    fix: .env logic

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await dotenv.load(fileName: ".env");
+
   runApp(const DishLabApp());
 }
